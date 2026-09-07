6e704f6d752c4f8b5635a53d4e4a2e617207968a
Kasper <93494353+KasperK09@users.noreply.github.com>
2026-09-06 20:17:41 -0500

    added health insurance calulator and some more print statements

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -7,6 +7,11 @@ public class App {
 
     double hourlyRate = 16.78;
     double overtimeRate = 1.5; //multiplicative
+    double SocialSecuityTax = 0.06;
+    double FederalIncomeTax = 0.14;
+    double StateIncomeTax = 0.05;
+    double UnionDues = 10.00;
+    double HealthInsurance;
 
     System.out.println("Welcome to the Payroll Program!");
     System.out.println("How many hours did you work this week?");
@@ -20,24 +25,43 @@ public class App {
 
     double GrossPayCalculated = calculateGrossPay(hourlyRate, overtimeRate, numberOfHoursWorked);
 
+    double HealthInsuranceCalculated = calculateHealthInsurancePayment(numberOfChildren);
+
     scanner.close();
 
     System.out.println("PayRoll Stub:\n");
 
     System.out.println("hours: " + numberOfHoursWorked);
     System.out.println("Rate: " + hourlyRate);
-    System.out.println("Overtime: " );
-    System.out.println("Gross: " + GrossPayCalculated + "\n"); //need to add this class
+    System.out.println("Gross: " + GrossPayCalculated); //need to add this class
 
-    
+    System.out.println("Taxes:\n");
+
+    System.out.println("Social Securuty Tax: " + SocialSecuityTax);
+    System.out.println("Federal Income Tax: " + FederalIncomeTax);
+    System.out.println("State Income Tax" + StateIncomeTax);
+
+    System.out.println("Other Payments:\n");
+
+    System.out.println("Union dues:" + StateIncomeTax);
+    System.out.println("Health Insurance:" + StateIncomeTax);
+
   }
-}
 
-public static double calculateGrossPay(double hourlyRate, double overtimeRate,double numberOfHoursWorked){
+  public static double calculateGrossPay(double hourlyRate, double overtimeRate,double numberOfHoursWorked){
   
-  if (numberOfHoursWorked <= 40) {
-    return numberOfHoursWorked * hourlyRate;
-  } else {
-    return ((numberOfHoursWorked * 40) * hourlyRate) + ((numberOfHoursWorked - 40) * (hourlyRate * overtimeRate));
+    if (numberOfHoursWorked <= 40) {
+      return numberOfHoursWorked * hourlyRate;
+    } else {
+      return ((numberOfHoursWorked * 40) * hourlyRate) + ((numberOfHoursWorked - 40) * (hourlyRate * overtimeRate));
+    }
+  }
+
+  public static int calculateHealthInsurancePayment(int numberOfChildren){
+    if (numberOfChildren < 3) {
+      return 15;
+    } else {
+      return 35;
+    }
   }
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -1,14 +1,14 @@
-package org.example;
+// package org.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-  // You can put your JUnit tests here
-  // Feel free to create other files, as needed, to organize your tests
+// class AppTest {
+//   // You can put your JUnit tests here
+//   // Feel free to create other files, as needed, to organize your tests
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
-}
+//   @Test
+//   void itWorks() {
+//     assertEquals(true, true);
+//   }
+// }
